69cc4ddfe6c5f230fcc2aee7f68200190f9d5913
Jean Pierre <jeangume1802@gmail.com>
2023-06-14 12:30:54 -0500

    Initial commit

---
+++ src/test/java/com/upc/javaejercicio/JavaEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.javaejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEjercicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
