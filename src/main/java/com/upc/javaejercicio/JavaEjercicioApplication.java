bf0e74162dce0b044e2f20de44f9b6a9db2409e9
Jean Pierre <jeangume1802@gmail.com>
2023-06-16 17:19:01 -0500

    Update

--- src/main/java/com/upc/javaejercicio/JavaEjercicioApplication.java
+++ src/main/java/com/upc/javaejercicio/JavaEjercicioApplication.java
@@ -2,11 +2,13 @@ package com.upc.javaejercicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "javaejercicio.azurewebsites.net")
 public class JavaEjercicioApplication {
 
     @GetMapping("/prueba")
